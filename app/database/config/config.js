8b7d938a9b7b75ce3cc1b3a834c3481400a85549
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-11-20 18:48:08 -0300

    deploy - debugging url.

--- app/database/config/config.js
+++ app/database/config/config.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 console.log('DATABASE_URL', process.env.DATABASE_URL);
 console.log('JAWSDB_URL', process.env.JAWSDB_URL);
+console.log('URL', process.env.URL);
 console.log('ENV', process.env.NODE_ENV);
 
 module.exports = {
