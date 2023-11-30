ad03aa28561b3a43da6b3b9a637a2f025a9e0732
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-11-30 19:58:31 -0300

    changes credentials for planetscale database connection.

--- app/database/config/config.js
+++ app/database/config/config.js
@@ -16,7 +16,12 @@ module.exports = {
     dialect: 'mysql'
   },
   production: {
-    use_env_variable: 'PLANET_DB_URL' || 'DATABASE_URL' || 'JAWSDB_URL' || 'URL',
+    // use_env_variable: 'PLANET_DB_URL' || 'DATABASE_URL' || 'JAWSDB_URL' || 'URL',
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
+    ssl: process.env.MYSQL_ATTR_SSL_CA,
     dialect: 'mysql',
   }
 }
