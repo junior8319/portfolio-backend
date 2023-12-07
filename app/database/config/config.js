3705fbfe16298854a0c263d00bc1616551c9c633
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-12-07 18:52:09 -0300

    change to a database deployed with railway.

--- app/database/config/config.js
+++ app/database/config/config.js
@@ -17,6 +17,10 @@ module.exports = {
   },
   production: {
     use_env_variable: 'DATABASE_URL' || 'JAWSDB_URL' || 'URL' || 'PLANET_DB_URL',
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
     dialect: 'mysql',
   }
 }
