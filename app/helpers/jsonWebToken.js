fe4980883c76c1fa547c7a5d22529a5786021779
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-12-17 17:28:15 -0300

    turns the function verifyToken() into synchronous; puts the options of jwt on a object.

--- app/helpers/jsonWebToken.js
+++ app/helpers/jsonWebToken.js
@@ -1,27 +1,22 @@
 const jwt = require('jsonwebtoken');
-const fs = require('fs');
 require('dotenv').config();
 
 const secretKey = process.env.TOKEN_SECRET;
 
+const jwtOptions = {
+  expiresIn: '1d',
+  algorithm: 'HS256',
+};
+
 const generateToken = (user) => {
-  const token = jwt.sign(user, secretKey, { expiresIn: '24h' });
+  const token = jwt.sign(user, secretKey, jwtOptions);
 
   return token;
 };
 
 const verifyToken = (token) => {
-  const decoded = jwt.verify(
-    token,
-    secretKey,
-    (err, decoded) => {
-      if (err) {
-        console.log('ERROR', err);
-        return err;
-      }
-      return decoded;
-    }
-  );
+  token = token.replace('Bearer ', '');
+  const decoded = jwt.verify(token, secretKey);
 
   return decoded;
 };
